566baec5a620e1fff23c820d68a9ddad8d6aa84f
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-08 08:58:48 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,9 +3,9 @@ public class Solution {
 
     static final int MODULO = (int) Math.pow(10, 9) + 7;
 
-    public int countOrders(int n) {
+    public int countOrders(int totalOrders) {
         long orderSequences = 1;
-        for (int orderNumber = 1; orderNumber <= n; orderNumber++) {
+        for (int orderNumber = 1; orderNumber <= totalOrders; orderNumber++) {
             orderSequences *= orderNumber * (2 * orderNumber - 1);
             orderSequences %= MODULO;
         }
